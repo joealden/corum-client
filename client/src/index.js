9d880eb5b2908203c6735dd962cd3dfc715f7cf6
Joe Alden <joerobertalden@gmail.com>
2017-08-30 17:49:51 +0100

    Add basic Apollo and router support

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router";
+import {
+  ApolloProvider,
+  createNetworkInterface,
+  ApolloClient
+} from "react-apollo";
 
-import './index.css';
-import App from './App';
-import registerServiceWorker from './utils/registerServiceWorker';
+import registerServiceWorker from "./utils/registerServiceWorker";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//TODO: Get GraphQL endpoint after talk to Maria about Graphcool use
+const networkInterface = createNetworkInterface({ uri: "PLACEHOLDER" });
+const client = new ApolloClient({ networkInterface });
+
+render(
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
