741cb17361677fe13fae7b3c23d7bbabebcb0402
Joe Alden <joerobertalden@gmail.com>
2017-08-30 23:29:49 +0100

    Remove styled-components from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import styled from "styled-components";
 
 const App = () => (
   <div>
     <p>Placeholder</p>
   </div>
 );
-
-const AppWrapper = styled.div`
-  color: red;
-  background-color: #44;
-`;
 
 export default withRouter(App);
