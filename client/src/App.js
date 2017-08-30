db024edeceaab65f6a62b0b0b58538e39a257e6b
Joe Alden <joerobertalden@gmail.com>
2017-08-30 23:23:27 +0100

    Add temp lint stage for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import styled from "styled-components";
 
 const App = () => (
   <div>
     <p>Placeholder</p>
   </div>
 );
+
+const AppWrapper = styled.div`
+  color: red;
+  background-color: #44;
+`;
 
 export default withRouter(App);
