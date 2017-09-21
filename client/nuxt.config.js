e7de93ea7c70a4f7341868b041d1c66c89eb862c
Joe Alden <joerobertalden@gmail.com>
2017-09-21 22:36:59 +0100

    Change default title

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Headers of the page
   head: {
-    title: 'corum-client',
+    title: 'Corum',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
