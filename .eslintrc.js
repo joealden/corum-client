723d30a76da502d41365fb7d76cb0e3e871864a2
Joe Alden <joerobertalden@gmail.com>
2017-09-21 15:35:39 +0100

    Change eslint config from standard to airbnb-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,29 @@
 module.exports = {
   root: true,
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
   env: {
     browser: true,
     node: true
   },
-  extends: "standard",
+  extends: 'airbnb-base',
   // required to lint *.vue files
-  plugins: ["html"],
+  plugins: ['html'],
   // add your custom rules here
-  rules: {},
-  globals: {}
+  rules: {
+    // don't require .vue extension when importing
+    'comma-dangle': ['error', 'never'],
+    'import/no-unresolved': 0,
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        optionalDependencies: ['test/unit/index.js']
+      }
+    ],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
 };
