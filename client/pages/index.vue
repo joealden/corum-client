db674cca957746c8c34545e27dd32f0733e04246
Joe Alden <joerobertalden@gmail.com>
2017-09-21 15:59:35 +0100

    Remove default pages

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,18 +1,6 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        corum-client
-      </h1>
-      <h2 class="subtitle">
-        An open, democratic &amp; self governing forum.
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+  <section>
+    <p>Test</p>
   </section>
 </template>
 
