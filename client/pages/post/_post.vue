939fba1f099d3b7c8472e538a54581eca6139c30
Joe Alden <joerobertalden@gmail.com>
2017-09-24 16:40:13 +0100

    Create post stub

---
+++ client/pages/post/_post.vue
@@ -0,0 +1,34 @@
+<template>
+<main>
+  <h1>Currently Selected Post</h1>
+  <p>{{ $route.params.post }}</p>
+</main>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Post' // TODO: wire up to post title
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+h1 {
+  text-decoration: underline;
+  font-size: 2.5rem;
+}
+
+p {
+  font-size: 30px;
+  text-transform: capitalize;
+}
+</style>
