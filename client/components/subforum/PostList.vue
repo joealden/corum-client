a6ae1519557ba4df2dac1c84fec91acbcf32fa76
Joe Alden <joerobertalden@gmail.com>
2017-09-24 21:23:22 +0100

    Add example post list to subforum

---
+++ client/components/subforum/PostList.vue
@@ -0,0 +1,80 @@
+<template>
+<ul id="post-list">
+  <li v-for="post in posts" :key="post.id">
+    <nuxt-link :to="'/post/' + post.id" class="post">
+      <div class="vote-count">{{ post.voteCount }}</div>
+      <div>
+        {{ post.title }}
+      </div>  
+    </nuxt-link> 
+  </li>
+</ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [ // dummy post data, will be fetched from GraphQL server
+        { id: 'a', title: 'Test 1 this is a test sentence', voteCount: 21 },
+        { id: 'b', title: 'Test 2 this is a test sentence', voteCount: 3 },
+        { id: 'c', title: 'Test 3 this is a test sentence', voteCount: 10 },
+        { id: 'd', title: 'Test 4 this is a test sentence', voteCount: 2003 },
+        { id: 'e', title: 'Test 5 this is a test sentence', voteCount: 102 },
+        { id: 'f', title: 'Test 6 this is a test sentence', voteCount: 0 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 },
+        { id: 'g', title: 'Test 7 this is a test sentence', voteCount: -43 }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#post-list {
+  margin: 0 auto;
+  padding: 1rem 1.5rem 0.6rem 1.5rem;
+  height: 100%;
+  overflow: auto;
+  width: 93%;
+  background-color: white;
+  border-left: 2px solid black;
+  border-right: 2px solid black;
+
+  @media screen and (min-width: 1800px) {
+    width: 1500px;
+  }
+}
+
+li {
+  list-style: none;
+}
+
+.post {
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.6rem;
+  padding: 1rem;
+}
+</style>
+
--- client/pages/subforum/_subforum.vue
+++ client/pages/subforum/_subforum.vue
@@ -1,12 +1,26 @@
 <template>
 <main>
-  <h1>Currently Selected Subforum</h1>
-  <p>{{ $route.params.subforum }}</p>
+  <h1>{{ $route.params.subforum }}</h1>
+  <div id="toolbar">
+    <form>
+      <input type="radio" id="popular" name="sort" value="popular" checked>
+      <label for="popular">Most Popular</label>
+      <input type="radio" id="new" name="sort" value="new">
+      <label for="new">Newest</label>
+    </form>
+    <nuxt-link to="/new/post">New Post</nuxt-link>
+  </div>
+  <post-list />
 </main>
 </template>
 
 <script>
-export default {
+import PostList from '~/components/subforum/PostList.vue';
+
+export default { // $route.params.subforum
+  components: {
+    PostList
+  },
   head() {
     return {
       title: 'Subforum' // TODO: wire up to subforum name
@@ -19,16 +33,62 @@ export default {
 main {
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  background-color: #eee;
+  overflow: hidden;
 }
 
 h1 {
-  text-decoration: underline;
-  font-size: 2.5rem;
+  font-size: 2rem;
+  text-transform: capitalize;
+  margin: 0 auto;
+  padding: 0.75rem 1.5rem;
+  background-color: black;
+  color: white;
+  width: 93%;
+  border-left: 2px solid black;
+  border-right: 2px solid black;
+
+  @media screen and (min-width: 1800px) {
+    width: 1500px;
+  }
 }
 
-p {
-  font-size: 30px;
-  text-transform: capitalize;
+#toolbar {
+  margin: 0 auto;
+  padding: 0.5em 1.5rem;
+  font-size: 1.4rem;
+  display: flex;
+  justify-content: space-between;
+  width: 93%;
+  align-items: center;
+  background-color: #ddd;
+  border-left: 2px solid black;
+  border-right: 2px solid black;
+  border-bottom: 2px solid black;
+
+  @media screen and (min-width: 1800px) {
+    width: 1500px;
+  }
+
+  form {
+    label[for="popular"] {
+      padding-right: 0.7rem;
+    }
+    label {
+      padding-left: 0.25rem;
+    }
+  }
+
+  a {
+    padding: 1rem;
+    border: none;
+    color: white;
+    background-color: black;
+    border-radius: 7px 7px 0 7px;
+    
+    &:hover {
+      background-color: #333;
+    }
+  }
 }
 </style>
