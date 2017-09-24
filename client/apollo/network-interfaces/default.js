41246b76a8a7488515c2fdf4aeca1321017e57d0
Joe Alden <joerobertalden@gmail.com>
2017-09-24 16:15:05 +0100

    Add nuxt apollo module

---
+++ client/apollo/network-interfaces/default.js
@@ -0,0 +1,9 @@
+// eslint disabled as @nuxt/apollo automatically adds as a dep
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createNetworkInterface } from 'apollo-client';
+
+export default () =>
+  createNetworkInterface({
+    // endpoint that points to the corum GraphQL backend on graph.cool
+    uri: 'https://api.graph.cool/simple/v1/cj7gqa4hb0iuj0114abl8ewcs'
+  });
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -30,5 +30,11 @@ module.exports = {
         });
       }
     }
+  },
+  modules: ['@nuxtjs/apollo'],
+  apollo: {
+    networkInterfaces: {
+      default: '~/apollo/network-interfaces/default.js'
+    }
   }
 };
