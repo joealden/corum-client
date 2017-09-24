9245de904610451c7a5ab1298d44f8646ea50b31
Joe Alden <joerobertalden@gmail.com>
2017-09-24 16:40:00 +0100

    Create 'New Post' page stub

---
+++ client/pages/new/post.vue
@@ -0,0 +1,28 @@
+<template>
+<main>
+  <h1>New Post Page Stub</h1>
+</main>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'New Post'
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+h1 {
+  text-decoration: underline;
+  font-size: 2.5rem;
+}
+</style>
