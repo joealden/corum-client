3fb5729a7790b29d4648c44b3305772fdcd137bc
Joe Alden <joerobertalden@gmail.com>
2017-09-25 22:23:50 +0100

    Replace dummy data with gql call

--- client/components/layout/Navigation.vue
+++ client/components/layout/Navigation.vue
@@ -3,8 +3,8 @@
   <h1>Subforums</h1>
   <input type="search" placeholder="Search...">
   <ul>
-    <li v-for="subforum in subforums" :key="subforum.id">
-      <nuxt-link :to="'/subforum/' + subforum.name">
+    <li v-for="subforum in allSubforums" :key="subforum.id">
+      <nuxt-link :to="`/subforum/${subforum.url}`">
         {{ subforum.name }}
       </nuxt-link>
     </li>
@@ -13,50 +13,14 @@
 </template>
 
 <script>
+import allSubforums from '~/apollo/queries/allSubforums.gql';
+
 export default {
-  data() {
-    return {
-      subforums: [
-        { id: 1, name: 'programming' },
-        { id: 2, name: 'gaming' },
-        { id: 3, name: 'linux' },
-        { id: 4, name: 'javascript' },
-        { id: 5, name: 'vuejs' },
-        { id: 6, name: 'reactjs' },
-        { id: 7, name: 'webdev' },
-        { id: 8, name: 'lineageos' },
-        { id: 9, name: 'nodejs' },
-        { id: 10, name: 'solusproject' },
-        { id: 11, name: 'tv' },
-        { id: 12, name: 'bestof' },
-        { id: 13, name: 'pics' },
-        { id: 14, name: 'movies' },
-        { id: 15, name: 'gifs' },
-        { id: 16, name: 'worldnews' },
-        { id: 17, name: 'funny' },
-        { id: 18, name: 'politics' },
-        { id: 19, name: 'books' },
-        { id: 20, name: 'videos' },
-        { id: 21, name: 'design' },
-        { id: 22, name: 'photoshop' },
-        { id: 23, name: 'angularjs' },
-        { id: 24, name: 'expressjs' },
-        { id: 25, name: 'gatsbyjs' },
-        { id: 26, name: 'nuxtjs' },
-        { id: 27, name: 'create-react-app' },
-        { id: 28, name: 'styled-components' },
-        { id: 29, name: 'css' },
-        { id: 30, name: 'postcss' },
-        { id: 31, name: 'postcss' },
-        { id: 32, name: 'postcss' },
-        { id: 33, name: 'postcss' },
-        { id: 34, name: 'postcss' },
-        { id: 35, name: 'postcss' },
-        { id: 36, name: 'postcss' },
-        { id: 37, name: 'postcss' },
-        { id: 38, name: 'postcss' }
-      ]
-    };
+  apollo: {
+    allSubforums: {
+      prefetch: true,
+      query: allSubforums
+    }
   }
 };
 </script>
