2f9a2c80d3b409fb24cc004709ad326713cab88b
Joe Alden <joerobertalden@gmail.com>
2017-10-03 12:45:05 +0100

    Clear placeholders on focus

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,17 +1,43 @@
 <template>
 <section>
-  <img src="~/assets/images/logo-light.svg" alt="corum">
+  <img 
+    src="~/assets/images/logo-light.svg"
+    alt="corum"
+  >
   <form>
-    <input type="email" placeholder="Email Address" autocapitalize="off" spellcheck="false">
-    <input type="password" placeholder="Password">
-    <input type="submit" value="Login" @click.prevent>
+    <input 
+      type="email" 
+      placeholder="Email Address" 
+      onfocus="this.placeholder=''" 
+      onblur="this.placeholder='Email Address'"
+      autocapitalize="off" 
+      spellcheck="false"
+    >
+    <input 
+      type="password" 
+      placeholder="Password"
+      onfocus="this.placeholder=''" 
+      onblur="this.placeholder='Password'"
+    >
+    <input 
+      type="submit" 
+      value="Login" 
+      @click.prevent="login"
+    >
   </form>
   <p>Don't have an account?<br><nuxt-link to="/signup">Sign Up</nuxt-link></p>
 </section>
 </template>
 
 <script>
-export default { head: () => ({ title: 'Login' }) };
+export default {
+  methods: {
+    login() {
+      // placeholder
+    }
+  },
+  head: () => ({ title: 'Login' })
+};
 </script>
 
 <style lang="scss" scoped>
--- client/pages/signup.vue
+++ client/pages/signup.vue
@@ -1,13 +1,38 @@
 <template>
 <section>
-  <img src="~/assets/images/logo-light.svg" alt="corum">
+  <img 
+    src="~/assets/images/logo-light.svg"
+    alt="corum"
+  >
   <form>
-    <input type="email" placeholder="Email Address" autocapitalize="off" spellcheck="false">
-    <input type="password" placeholder="Password">
-    <input type="password" placeholder="Confirm Password">
-    <input type="submit" value="Sign Up" @click.prevent>
+    <input 
+      type="email"
+      placeholder="Email Address"
+      onfocus="this.placeholder=''" 
+      onblur="this.placeholder='Email Address'"
+      autocapitalize="off"
+      spellcheck="false"
+    >
+    <input
+      type="password"
+      placeholder="Password"
+      onfocus="this.placeholder=''" 
+      onblur="this.placeholder='Password'"
+    >
+    <input 
+      type="password"
+      placeholder="Confirm Password"
+      onfocus="this.placeholder=''" 
+      onblur="this.placeholder='Confirm Password'"
+      onkeydown="if (event.keyCode == 13) { this.form.submit(); return false; }"
+    >
+    <input 
+      type="submit"
+      value="Sign Up"
+      @click.prevent
+    >
   </form>
-  <p>Already have an account? <br><nuxt-link to="/login">Login</nuxt-link></p>
+  <p>Already have an account?<br><nuxt-link to="/login">Login</nuxt-link></p>
 </section>
 </template>
 
