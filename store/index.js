abb956fc07e42b4305843adc1e4302087c793650
Joe Alden <joerobertalden@gmail.com>
2017-10-17 11:16:39 +0100

    Fix eslint warning about set export

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable */
 
 import { Store } from 'vuex'
-import { set } from 'vue'
+import Vue from 'vue'
 
 /* eslint-enable */
 // Vue.set() used to get around Vue's inability to detect the state change
@@ -21,21 +21,21 @@ export default () => {
         localStorage.removeItem('user-id')
         localStorage.removeItem('username')
         localStorage.removeItem('auth-token')
-        set(state, 'userId', localStorage.getItem('user-id'))
-        set(state, 'username', localStorage.getItem('username'))
+        Vue.set(state, 'userId', localStorage.getItem('user-id'))
+        Vue.set(state, 'username', localStorage.getItem('username'))
       },
 
       saveUserData(state, { id, username, token }) {
         localStorage.setItem('user-id', id)
         localStorage.setItem('username', username)
         localStorage.setItem('auth-token', token)
-        set(state, 'userId', localStorage.getItem('user-id'))
-        set(state, 'username', localStorage.getItem('username'))
+        Vue.set(state, 'userId', localStorage.getItem('user-id'))
+        Vue.set(state, 'username', localStorage.getItem('username'))
       },
 
       updateUserState(state) {
-        set(state, 'userId', localStorage.getItem('user-id'))
-        set(state, 'username', localStorage.getItem('username'))
+        Vue.set(state, 'userId', localStorage.getItem('user-id'))
+        Vue.set(state, 'username', localStorage.getItem('username'))
       }
     }
   })
