9f6ae2f99a0291608384f4fa823becfc8300c658
Joe Alden <joerobertalden@gmail.com>
2017-11-08 20:21:51 +0000

    Allow ternary operators where I want them

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,34 @@
 // For more info, visit https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
+	root: true,
+	parser: 'babel-eslint',
 
-  parserOptions: {
-    sourceType: 'module'
-  },
+	parserOptions: {
+		sourceType: 'module'
+	},
 
-  env: {
-    browser: true,
-    node: true
-  },
+	env: {
+		browser: true,
+		node: true
+	},
 
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: ['html'],
+	extends: 'standard',
+	// required to lint *.vue files
+	plugins: ['html'],
 
-  rules: {
-    semi: 0,
-    'space-before-function-paren': 0,
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': [
-      0,
-      {
-        optionalDependencies: ['test/unit/index.js']
-      }
-    ],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+	rules: {
+		semi: 0,
+		'no-unneeded-ternary': 0,
+		'space-before-function-paren': 0,
+		// allow optionalDependencies
+		'import/no-extraneous-dependencies': [
+			0,
+			{
+				optionalDependencies: ['test/unit/index.js']
+			}
+		],
+		// allow debugger during development
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+	}
 }
