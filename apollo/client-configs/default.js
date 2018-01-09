996e72454df55a1e00f11e250316ecab99787f94
Joe Alden <joerobertalden@gmail.com>
2018-01-09 11:52:18 +0000

    Move token fetch inside of middleware to update request headers without hard page refresh

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -2,17 +2,9 @@ import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-export default ctx => {
+export default () => {
   const uri = process.env.API_ENDPOINT
   const httpLink = new HttpLink({ uri })
-
-  /*
-    Due to this being parsed SSR, process.browser must be used to
-    make sure localStorage is only attempted to be accessed on the
-    client. This isn't an issue as no queries or mutations that
-    require authentaction are made on an SSR request of the page.
-  */
-  const token = process.browser ? localStorage.getItem('auth-token') : undefined
 
   /*
     This middleware sends the JWT (if present) that is needed for
@@ -20,6 +12,16 @@ export default ctx => {
     in users are allowed to peform such mutations.
   */
   const middlewareLink = new ApolloLink((operation, forward) => {
+    /*
+      Due to this being parsed SSR, process.browser must be used to
+      make sure localStorage is only attempted to be accessed on the
+      client. This isn't a  n issue as no queries or mutations that
+      require authentaction are made on an SSR request of the page.
+    */
+    const token = process.browser
+      ? localStorage.getItem('auth-token')
+      : undefined
+
     operation.setContext({
       headers: { authorization: `Bearer ${token}` }
     })
