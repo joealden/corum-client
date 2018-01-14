93439ddad19e66f675e38584aedee59e97f15648
Joe Alden <joerobertalden@gmail.com>
2018-01-14 19:08:07 +0000

    Revert landing page to old

--- assets/markdown/loggedIn.md
+++
@@ -1,6 +0,0 @@
-# Corum User Guide
-
-## Getting Back to This Page
-
-If you need help at anytime, simply click on the 'Corum' logo in the top right of the site
-to return to this page.
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,21 @@
 <template>
 <section>
-  <div v-html="userId ? loggedIn : notLoggedIn"></div>
+  <img src="~/assets/images/logo-dark.svg" alt="corum">
+  <p>Please Select a Subforum!</p>
 </section>
 </template>
 
 <script>
-import loggedIn from '~/assets/markdown/loggedIn.md'
-import notLoggedIn from '~/assets/markdown/notLoggedIn.md'
-
 export default {
-  data() {
-    return { loggedIn, notLoggedIn }
-  },
-
-  computed: {
-    // Gets the user's ID from the vuex store
-    userId() {
-      return this.$store.state.userId
-    }
-  },
-
   head: () => ({ title: 'Home' })
 }
 </script>
 
 <style lang="stylus" scoped>
-@require '../assets/styles/variables'
+img
+  height 6rem
 
-section
-  flex-grow 1
-  margin 2rem
-  display flex
-  flex-direction column
-  font-size 1.5rem
-  background-color white
-  border-radius 0.5rem
-  box-shadow 10px 10px 25px #999
-
-  div
-    h1
-      text-decoration underline
-
-    a
-      color $nav-hover
-
-      &:hover, &:focus
-        text-decoration underline
+p
+  font-size 1.7rem
+  font-weight bold
 </style>
